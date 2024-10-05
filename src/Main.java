46de4cd053a20fbad1e5f52f886cca8d999013f5
Yari-i <tipo47181@gmail.com>
2024-10-05 14:36:00 +0500

    HW Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import exeptions.WrongLoginException;
+import exeptions.WrongPasswordException;
+
+import static registerUser.UserRegistration.registerUser;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            registerUser("yari1234", "123456789", "123456789");
+            System.out.println("Пользователь успешно зарегистрирован!");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.err.println("Ошибка регистрации: " + e.getMessage());
+        }
     }
 }
